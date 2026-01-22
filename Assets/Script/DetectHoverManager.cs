defbe06e9993528d20e91f122c2f013aa309e73f
porotika6 <porotika6@gmail.com>
2026-01-22 20:09:49 +0700

    Input Script Detect Hover

---
+++ Assets/Script/DetectHoverManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class DetectHovrManager : MonoBehaviour
+{
+     private SpriteRenderer sr;
+    private bool isHovered;
+    private Camera cam;
+    private Sprite defaultSprite;
+    public Sprite hoverSprite;
+ 
+    
+    // Update is called once per frame
+    void Update()
+    {
+         DetectHover();
+    }
+
+    void DetectHover()
+    {
+        Vector2 mouseWorldPos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            if (!isHovered)
+            {
+                isHovered = true;
+                  if (hoverSprite != null) sr.sprite = hoverSprite;
+            }
+        }
+        else if (isHovered)
+        {
+            isHovered = false;
+              if (sr != null) sr.sprite = defaultSprite;
+        }
+    }
+}
