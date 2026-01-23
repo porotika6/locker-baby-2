570e7c991391d298eed22d4dabe88323acd4494c
kh4iri <khairinajahrifky12@gmail.com>
2026-01-23 11:49:35 +0700

    Create Transition prefab

--- Assets/Script/Transition.cs
+++ Assets/Script/Transition.cs
@@ -1,27 +1,8 @@
 using UnityEngine;
 
-public class Transition : MonoBehaviour
+public class Transition : MonoBehaviourSingleton<Transition>
 {
-    public static Transition Instance { get; private set; }
-
     [SerializeField] private Animator _animator;
-
-    private void Awake()
-    {
-        // Cek apakah Instance sudah ada
-        if (Instance != null && Instance != this)
-        {
-            // Jika sudah ada instance lain, hancurkan objek ini (duplikat)
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-        
-        // Mengambil komponen di Awake lebih aman untuk Singleton
-        _animator = GetComponent<Animator>();
-    }
 
     public void PlayFadeAnimation()
     {
